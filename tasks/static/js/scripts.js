aa4ac758a3d91c5b85600c496b720eab7951e3bc
devPedro98 <pedroribeirobarrios@gmail.com>
2022-05-16 17:40:41 -0300

    filter by status

--- tasks/static/js/scripts.js
+++ tasks/static/js/scripts.js
@@ -1,7 +1,9 @@
 $(document).ready(function() {
+    var baseUrl = 'http://127.0.0.1:8000/';
     var deleteBtn = $('.delete-btn');
     var searchBtn = $('#search-btn');
     var searchForm = $('#search-form');
+    var filter     = $('#filter') ;
 
     $(deleteBtn).on('click', function(e) {
         e.preventDefault();
@@ -16,5 +18,11 @@ $(document).ready(function() {
     $(searchBtn).on('click', function() { 
         searchForm.submit();
     });
+
+    $(filter).change(function() { 
+        var filter = $(this).val();
+        window.location.href = baseUrl + '?filter=' + filter;
+    })
+    
 
 });
--- tasks/views.py
+++ tasks/views.py
@@ -11,8 +11,12 @@ from .models import Task
 @login_required
 def taskList(request):
     search = request.GET.get('search')
+    filter = request.GET.get('filter')
+
     if search:
         tasks = Task.objects.filter(title__icontains=search, user=request.user)
+    elif filter:
+        tasks = Task.objects.filter(done=filter, user=request.user)
     else:
         tasks_list = Task.objects.all().order_by('-created_at').filter(user=request.user) # noqa
         paginator = Paginator(tasks_list, 6)
