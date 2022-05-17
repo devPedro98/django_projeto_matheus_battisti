4e40ab208b2268df113f056607e17c6b95777827
devPedro98 <pedroribeirobarrios@gmail.com>
2022-05-17 07:56:51 -0300

    new changes about

--- helloworld/urls.py
+++ helloworld/urls.py
@@ -19,7 +19,7 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('tasks.urls')),
+    path('about/', include('about.urls')),
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('accounts/about/', include('about.urls')),
 ]
