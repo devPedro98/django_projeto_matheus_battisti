8a01f138dc5bc0b1bbeece48705a9fc22878f993
devPedro98 <pedroribeirobarrios@gmail.com>
2022-05-17 07:47:49 -0300

    about

--- helloworld/urls.py
+++ helloworld/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('', include('tasks.urls')),
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/about/', include('about.urls')),
 ]
